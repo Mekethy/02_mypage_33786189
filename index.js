6648ed21d9243241951789be8e583b2227a70da7
Kenny Hoang <kennyhoang1404@gmail.com>
2025-10-15 09:19:37 +0100

    made html file + file to show the link for page

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var http = require("http");
 const port = 8000;
 
 http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!");
+  fs.readfile ("index.html", function (err, data) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+})
 }).listen(port, function () {
   console.log(`Node server is running on port ${port}...`); //writes to console when server starts
 });
